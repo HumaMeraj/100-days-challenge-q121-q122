dc0918b9ba22e5d7e4503d4f8d2567dd97c2cf8d
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-05-29 00:47:08 +0500

    File of q122

---
+++ q122.js
@@ -0,0 +1,12 @@
+//Use a while loop to count down from 10 to 1 and breaks the loop when it reaches 5.
+// Initializes the counter at 10
+var counter = 10;
+// This while loop counts down from 10 to 1 and stops at 5
+while (counter > 0) {
+    if (counter === 5) {
+        break; // Exits the loop when counter reaches 5
+    }
+    console.log(counter);
+    counter--;
+}
+// Logs the countdown from 10 but stops abruptly when it hits 5.
